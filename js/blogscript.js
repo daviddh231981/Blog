6238c32bb1731d45e57bdfe6be7443f06b7e346d
daviddh231981 <d231981@outlook.com>
2022-08-20 15:59:18 -0500

    Nav modification, color scheme redesign

--- js/blogscript.js
+++ js/blogscript.js
@@ -34,6 +34,24 @@ function showTrip() {
 
 }
 
+//Dropdown menu
+//when button is selected, show navigation.
+function showNav() {
+
+  //NAV variables
+  const navShowContents = document.querySelector("nav");
+  const btnShowNavigation = document.getElementById("navBtn");
+
+  if (navShowContents.style.display === "block") {
+      navShowContents.style.display = "none";
+      btnShowNavigation.innerHTML = "Show<br>Menu"; 
+      topFunction();      
+  } else {
+      navShowContents.style.display = "block"
+      btnShowNavigation.innerHTML = "Hide<br>Menu";
+  }
+}
+
 //load lower resolution images
 (() => {
   'use strict';
